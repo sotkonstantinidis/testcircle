8e33a6e46c72e9d61a4d1c578baf295e4dd8a7b7
lvonlanthen <lukas.vonlanthen@cde.unibe.ch>
2014-08-05 16:59:17 +0200

    Added first template based on Foundation. Also created language switcher

--- qcat/urls.py
+++ qcat/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'qcat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+
+    url(r'^$', 'qcat.views.home', name='home'),
+
+    # View to change language
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 
     # url(r'^admin/', include(admin.site.urls)),
 )
