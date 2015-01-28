3f4b82e17767416dc449488d10d6e2810a130b60
Lukas Vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2015-01-28 07:54:12 +0100

    Introduce bower and grunt, design by WeAreCube

--- qcat/settings.py
+++ qcat/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'qcat', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 TEMPLATE_DIRS = (
