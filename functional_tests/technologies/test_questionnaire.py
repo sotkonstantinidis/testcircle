724c2d2d3470cbf813a42e02b3c32cc724350988
Lukas Vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2015-07-13 14:18:29 +0200

    Fixed translation bug

--- functional_tests/technologies/test_questionnaire.py
+++ functional_tests/technologies/test_questionnaire.py
@@ -33,6 +33,11 @@ class QuestionnaireTest(FunctionalTest):
             'xpath', '//div[@class="tech-section-progress progress"]')
         self.assertEqual(len(progress_indicators), get_category_count())
 
+        # She sees that all the categories are there with their correct name
+        # Except the first one which is not displayed in the header template
+        for category in [c[1] for c in get_categories()][1:]:
+            self.findBy('xpath', '//h2[text() = "' + category + '"]')
+
         # She goes to the first step and sees the link works.
         self.browser.get(self.live_server_url + reverse(
             route_questionnaire_new_step, args=[get_categories()[0][0]]))
