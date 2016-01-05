3af152150e610eeb2a4929bb572684fb858da8ce
Sebastian Manger <s.manger@gmx.ch>
2016-01-05 13:24:35 +0100

    733 Allow csv for rendering the api results.

--- qcat/settings.py
+++ qcat/settings.py
@@ -191,6 +191,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
         'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_csv.renderers.CSVRenderer',
     ),
 }
 
