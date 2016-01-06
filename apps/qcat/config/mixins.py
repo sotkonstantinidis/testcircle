5f4c7e659b61bc737198151d243eff10bb26d05e
Sebastian Manger <s.manger@gmx.ch>
2016-01-06 16:49:56 +0100

    Add security mixin.

--- apps/qcat/config/mixins.py
+++ apps/qcat/config/mixins.py
@@ -13,6 +13,21 @@ class ProdMixin:
     TEMPLATE_DEBUG = values.BooleanValue(False)
 
 
+class SecurityMixin:
+    # Security settings, as recommended from manage.py check --deploy
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+    # It is recommended to set the max-age to a big value like 12/24 months
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    # Turn this on as soon as the site is available with ssl.
+    # SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_HTTPONLY = True
+
+
 class OpBeatMixin:
     @property
     def INSTALLED_APPS(self):
--- apps/qcat/settings.py
+++ apps/qcat/settings.py
@@ -1,10 +1,10 @@
 from .config.common import BaseSettings
-from .config.mixins import DevMixin, ProdMixin, OpBeatMixin
+from .config.mixins import DevMixin, ProdMixin, OpBeatMixin, SecurityMixin
 
 
 class DevDefaultSite(DevMixin, BaseSettings):
     pass
 
 
-class ProdDefaultSite(ProdMixin, OpBeatMixin, BaseSettings):
+class ProdDefaultSite(ProdMixin, OpBeatMixin, SecurityMixin, BaseSettings):
     pass
