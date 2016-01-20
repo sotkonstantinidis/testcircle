3da5a98f9e121846a79b8c4c33298151fdde4ebd
Sebastian Manger <s.manger@gmx.ch>
2016-01-20 12:14:05 +0100

    Add serverconfig for qcat-dev.

--- apps/qcat/wsgi.py
+++ apps/qcat/wsgi.py
@@ -6,5 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
+import sys
+import envdir
+from os.path import join, dirname, abspath
+
+# Root directory for this project
+QCAT_DIR = dirname(dirname(dirname(abspath(__file__))))
+
+# Add the apps folder to the path and read the env-vars.
+sys.path.append(join(QCAT_DIR, 'apps'))
+envdir.read(join(QCAT_DIR, 'envs'))
+
+# Load the wsgi application with django-configurations.
 from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
