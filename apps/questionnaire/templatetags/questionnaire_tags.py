f92c90ce0a2f2cb5be188ce4af468b02950e94de
Sebastian Manger <s.manger@gmx.ch>
2016-02-01 12:35:44 +0100

    759 Show/hide the edit button depending on the user and the questionnaires status. This will be applied to all templates.

--- apps/questionnaire/templatetags/questionnaire_tags.py
+++ apps/questionnaire/templatetags/questionnaire_tags.py
@@ -4,18 +4,22 @@ from questionnaire.models import Questionnaire
 register = template.Library()
 
 
-@register.simple_tag
-def editable_css_class(questionnaire, user):
+@register.assignment_tag
+def call_model_method(method, obj, user):
     """
+    Call a method on the Questionnaire model with given user and return its
+    result.
 
     Args:
-        questionnaire: questionnaire.models.Questionnaire
-        user: accounts.models.User
+        method: string (method to call)
+        obj: questionnaire.models.Questionnaire
+        user: accounts.User
 
     Returns:
-        string: 'readonly' if questionnaire can't be edited by given user.
+        string: result of the called function
 
     """
-    if isinstance(questionnaire, Questionnaire):
-        return questionnaire.edit_css_class(user)
+    if isinstance(obj, Questionnaire):
+        result = getattr(obj, method)(user)
+        return result if not isinstance(result, tuple) else result[1]
     return ''
