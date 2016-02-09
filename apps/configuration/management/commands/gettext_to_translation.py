08a382f0fab57daa9b8ad0c43d437f1c08aa6889
Sebastian Manger <s.manger@gmx.ch>
2016-02-09 12:26:42 +0100

    823 Crude functional version.

--- apps/configuration/management/commands/gettext_to_translation.py
+++ apps/configuration/management/commands/gettext_to_translation.py
@@ -16,16 +16,11 @@ class Command(NoArgsCommand):
 
     """
     filename = 'extract'
-    translations = collections.defaultdict()
-    language_structure = {
-        'model_info': [],
-        'text': ''
-    }
+    translations = collections.defaultdict(dict)
 
     def handle_noargs(self, **options):
         """
         Read translated files and write contents into the Translation model.
-
         """
         languages = dict(settings.LANGUAGES).keys()
         for language in languages:
@@ -34,20 +29,64 @@ class Command(NoArgsCommand):
             )
             with contextlib.suppress(FileNotFoundError):
                 with open(join(path, '{}.po'.format(self.filename))) as f:
-                    for line in self.get_translations(f.readlines()):
-                        print(line)
+                    self.get_translations(f.readlines())
+
+        self.restore_to_orm()
 
     def get_translations(self, lines):
         """
+        Parse the lines: extract information about models that is stored in the
+        comments and the translated text. The result is written to
+        self.translations.
+
         Args:
             lines: list of strings
-
-        Returns:
-            iterator: only lines that indicate model contents.
-
         """
-        last_model_obj = None
+        pk = None
+        bfr = []
         for line in lines:
+            # For all lines that are a comment: extract information to a buffer.
             if re.match('# (\d+)', line):
                 comment = line[2:].rstrip('\n').split('.')
-                yield comment
+                bfr.append(comment[1:])
+                pk = comment[0]
+
+            # If the msgstr is emtpy, take the value from the msgid.
+            elif line.startswith('msgid') and pk:
+                msgid = line
+
+            # After comments, the actual text is found. Copy the text and buffer
+            # to the global dict and empty the buffer.
+            # When appending the buffer to the global dict, manage the structure
+            # so that inserting into the orm is easy.
+            elif line.startswith('msgstr') and pk:
+                text = self.extract_text(line) or self.extract_text(msgid)
+                for item in bfr:
+                    # The innermost element must contain the text, all other
+                    # keys are just dict keys wrapping the text.
+                    pointer = self.translations[pk]
+                    for key in item[:-1]:
+                        pointer = pointer.setdefault(key, {})
+                    pointer.update({item[-1]: text})
+                pk = None
+                bfr = []
+
+    @staticmethod
+    def extract_text(line):
+        """
+        Args:
+            line: string
+
+        Returns: extract translated content from string with msgid, msgstr.
+        """
+        return line[line.find('"')+1:line.rfind('"')]
+
+    def restore_to_orm(self):
+        """
+        Write data from self.translations back into the model.
+
+        """
+        for pk, data in self.translations.items():
+            translation = Translation.objects.get(pk=pk)
+            translation.data = data
+            translation.save()
