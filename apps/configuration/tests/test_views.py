ad01d025699652e94b8bb0987611794bc512abf6
Sebastian Manger <s.manger@gmx.ch>
2016-03-02 14:06:49 +0100

    837 Cache all configurations at once

---
+++ apps/configuration/tests/test_views.py
@@ -0,0 +1,58 @@
+import contextlib
+from unittest.mock import MagicMock
+
+from django.core.cache import cache
+from django.core.urlresolvers import reverse
+from django.test import RequestFactory, override_settings
+from django.utils.translation import get_language
+
+from qcat.tests import TestCase
+from ..cache import get_configuration
+from ..models import Configuration
+from ..views import BuildAllCachesView, delete_caches
+
+
+class CacheTest(TestCase):
+
+    fixtures = ['sample.json']
+    locmem = {'default': {'BACKEND': 'django.core.cache.backends.locmem.'
+                                           'LocMemCache'}}
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def setup_view(self, view, request, *args, **kwargs):
+        """
+        Mimic ``as_view()``, but returns view instance.
+        Use this function to get view instances on which you can run unit tests,
+        by testing specific methods.
+        """
+        view.request = request
+        view.args = args
+        view.kwargs = kwargs
+        return view
+
+    @override_settings(CACHES=locmem)
+    def test_build_cache(self):
+        """
+        Integration test: check valid cache after call to view.
+        """
+        request = self.factory.get(reverse('search:build_caches'))
+        request._messages = MagicMock()
+        view = self.setup_view(BuildAllCachesView(), request)
+        view.get(request)
+        self.assertIsNotNone(cache.get('sample_{}'.format(get_language())))
+
+    @override_settings(CACHES=locmem)
+    def test_clear_cache(self):
+        config = Configuration.objects.all().first()
+        get_configuration(config.code)
+        cache_key = '{}_{}'.format(config.code, get_language())
+        self.assertIsNotNone(cache.get(cache_key))
+        request = MagicMock()
+        request.user.is_superuser = True
+
+        # Missing messages-framework will throw a type error.
+        with contextlib.suppress(TypeError):
+            delete_caches(request)
+            self.assertIsNone(cache.get(cache_key))
--- apps/configuration/views.py
+++ apps/configuration/views.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import redirect
+from django.views.generic import RedirectView
+from django.utils.translation import ugettext_lazy as _
 
 from accounts.decorators import force_login_check
-from configuration.cache import delete_configuration_cache
+from configuration.cache import delete_configuration_cache, get_configuration
 from configuration.models import Configuration
 
 
@@ -31,3 +36,30 @@ def delete_caches(request):
     messages.success(request, 'Caches deleted.')
 
     return redirect('search:admin')
+
+
+class BuildAllCachesView(RedirectView):
+    """
+    Build cache for all questionnaires.
+    """
+    url = reverse_lazy('search:admin')
+    permanent = False
+
+    def get(self, request, *args, **kwargs):
+        self.build_caches(request)
+        return super().get(request, *args, **kwargs)
+
+    def build_caches(self, request):
+        """
+        Build the cache for all active configurations.
+
+        """
+        configurations = Configuration.objects.filter(active=True)
+        cached_configs = []
+        for configuration in configurations:
+            get_configuration(configuration.code)
+            cached_configs.append(configuration.code)
+
+        messages.success(request, _(u"Built cache for {}".format(
+            ', '.join(cached_configs)
+        )))
