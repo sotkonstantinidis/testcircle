a2bd6fac3a294a4d5d72127c44f39bc899913f25
Sebastian Manger <s.manger@gmx.ch>
2016-04-11 16:21:38 +0200

    871 Remove 'unccd' landing page

--- apps/unccd/urls.py
+++ apps/unccd/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, patterns
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'unccd.views.home', name='home'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('wocat:home')), name='home'),
     url(r'^view/(?P<identifier>\w+)/$', 'unccd.views.questionnaire_details',
         name='questionnaire_details'),
     url(r'^view/(?P<identifier>\w+)/(?P<step>\w+)/$',
