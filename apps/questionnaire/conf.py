c2fb625e2452bf2e3523da86433dfebc5dd2aa6e
Sebastian Manger <s.manger@gmx.ch>
2016-08-22 13:51:54 +0200

    1003 Change handling of fields:
    * Include unccd_description to mapped fields ('definition')
    * After change request from API consumer: change structure of name and definition; including the language.

--- apps/questionnaire/conf.py
+++ apps/questionnaire/conf.py
@@ -37,7 +37,8 @@ class QuestionnaireConf(AppConf):
         'flags',
     ]
 
-    # Definition = one sentence, description = max. 2500 chars
+    # Mapping for consistent field names on the API. See
+    # questionnaire.api.views.QuestionnaireAPIMixin for usage.
     API_CHANGE_KEYS = {
         'app_definition': 'definition',
         'tech_definition': 'definition',
