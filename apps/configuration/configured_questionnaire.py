54b5b4a658a5db348891bcd49f6c1088996a9c11
Sebastian Manger <s.manger@gmx.ch>
2016-10-24 16:53:09 +0200

    715 Refactor: use 'child' attribute instead of fixed list
     * questiongroups can consist of questiongroups
     * sketch for improved value gathering

--- apps/configuration/configured_questionnaire.py
+++ apps/configuration/configured_questionnaire.py
@@ -1,72 +1,53 @@
 import collections
 import functools
 
-from .configuration import QuestionnaireConfiguration, QuestionnaireQuestion
+from .configuration import QuestionnaireQuestion
 
 
 class ConfiguredQuestionnaire:
     """
     Combine given configuration and data into a single ordered dict.
     """
-    nodes = [
-        'sections',
-        'categories',
-        'subcategories',
-        'questiongroups',
-        'questions'
-    ]
     store = collections.OrderedDict()
     tmp_path = []  # all dict keys until the current element
-    tmp_values = {}  # store question-values for current questiongroup
 
     def __init__(self, config, **data):
         self.values = data
         self.values_keys = self.values.keys()
         self.get_children(config)
 
-    def get_children(self, config: QuestionnaireConfiguration, index=0):
+    def get_children(self, config):
         """
-        Walk through the configuration and build a 'store' with all nested
-        elements, adding the filled in values from self.data.
+        Walk through the configuration and build a 'store' with all nested and
+        sibling elements, adding the filled in values from self.data.
         """
-        children = getattr(config, self.nodes[index])
-        is_question = index >= len(self.nodes) - 1
+        children = getattr(config, 'children')
 
         for child in children:
-            # Reset the path for root elements.
-            if index == 1:
-                self.tmp_path = []
-
             # Append current values to the store
             self.active_child_in_store[child.keyword] = {
                 'label': str(child.label),
                 'children': {}
             }
-            # Temporary helpers; the filled in questions are grouped by its
-            # questiongroup.
-            if self.nodes[index] == 'questiongroups':
-                self.tmp_values = {}
-                if child.keyword in self.values_keys:
-                    # todo: discuss with lukas: if multiple list items are
-                    # available, what does this mean? the questiongroup consists
-                    # of the same question only?
-                    self.tmp_values = self.values[child.keyword][0]
-
-            # Add the 'path' to the current element, so the next iteraction can
-            # access this element and append to it.
-            self.tmp_path.extend([child.keyword, 'children'])
 
             # If the current element is not a question, iterate 'down' into the
             # next level of the config.
-            if not is_question:
-                self.get_children(child, index + 1)
-            else:
+            if isinstance(child, QuestionnaireQuestion):
                 self.put_question_data(child)
+            else:
+                # Add the 'path' to the current element, so the next iteration
+                # can access this element and append to it.
+                self.tmp_path.extend([child.keyword, 'children'])
+                self.get_children(child)
+
+        # Go back to the parent element.
+        self.tmp_path = self.tmp_path[:-2]
 
     def put_question_data(self, child):
+        value = self.values.get(child.parent_object.keyword)
         self.active_child_in_store[child.keyword] = {
             'label': str(child.label),
-            'value': self.tmp_values.get(child.keyword) or ''
+            'value': child.get_details(value[0]) if value else ''
         }
 
     @property
@@ -81,6 +62,7 @@ class ConfiguredQuestionnaireSummary(ConfiguredQuestionnaire):
     data = {}
 
     def put_question_data(self, child: QuestionnaireQuestion):
+        # add named 'part' in configuration
         if hasattr(child, 'is_in_summary') and child.is_in_summary:
             self.data.update({
                 '{}.{}'.format(child.questiongroup.keyword, child.keyword): {
