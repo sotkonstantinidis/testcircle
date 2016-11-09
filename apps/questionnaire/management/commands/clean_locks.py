a70263b2e1b153b6a34c03c076691a51f82756bf
Sebastian Manger <s.manger@gmx.ch>
2016-11-09 13:51:54 +0100

    1105 Fix management command

--- apps/questionnaire/management/commands/clean_locks.py
+++ apps/questionnaire/management/commands/clean_locks.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
 from django.core.management.base import NoArgsCommand
+from django.conf import settings
+from django.db.models import Q
+from django.utils.timezone import now
 from questionnaire.models import Lock
 
 
@@ -7,4 +11,7 @@ class Command(NoArgsCommand):
     Purge stale locks.
     """
     def handle_noargs(self, **options):
-        Lock.with_status.is_editable().delete()
+        expired = now() - timedelta(minutes=settings.QUESTIONNAIRE_LOCK_TIME)
+        Lock.objects.filter(
+            Q(is_finished=True) | Q(start__lte=expired)
+        ).delete()
