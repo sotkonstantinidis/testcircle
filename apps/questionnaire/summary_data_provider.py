598d504fdd06c0873d543924db5fab6c59047b15
Sebastian Manger <s.manger@gmx.ch>
2016-11-28 16:40:14 +0700

    715 Restore state for frontend-devs

--- apps/questionnaire/summary_data_provider.py
+++ apps/questionnaire/summary_data_provider.py
@@ -51,13 +51,13 @@ class SummaryDataProvider:
         Load full (raw) data in the same way that it is created for the API and
         apply data transformations to self.data.
         """
-        self.raw_data = ConfiguredQuestionnaireSummary(
-            config=config, summary_type=self.summary_type,
-            questionnaire=questionnaire, **data
-        ).data
-        self.questionnaire = questionnaire
-        self.data = dict(self.get_data())
-        #self.data = self.get_demo_dict()
+        # self.raw_data = ConfiguredQuestionnaireSummary(
+        #     config=config, summary_type=self.summary_type,
+        #     questionnaire=questionnaire, **data
+        # ).data
+        # self.questionnaire = questionnaire
+        # self.data = dict(self.get_data())
+        self.data = self.get_demo_dict()
 
     def get_data(self):
         """
