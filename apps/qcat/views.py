6b832612260b82a3d65dda089e794dee3ebee6d9
Sebastian Manger <s.manger@gmx.ch>
2017-01-25 18:57:54 +0100

    Basic structure

--- apps/qcat/views.py
+++ apps/qcat/views.py
@@ -1,6 +1,9 @@
 from django.contrib import sitemaps
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.generic import TemplateView
 
 
 def home(request):
@@ -31,3 +34,22 @@ class StaticViewSitemap(sitemaps.Sitemap):
 static_sitemap = {
     'static': StaticViewSitemap,
 }
+
+
+class FactsTeaserView(TemplateView):
+    """
+    Display some relevant numbers.
+    """
+    http_method_names = ['get']
+    template_name = 'qcat/templates/fact_sheet_teaser.html'
+
+    @method_decorator(cache_page(5))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        Request piwik-api here to protect auth token.
+        """
+        context = super().get_context_data(**kwargs)
+        return context
