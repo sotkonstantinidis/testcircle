52fa9a23abbb9ee38004b46b358b82d44ff6e35c
Sebastian Manger <s.manger@gmx.ch>
2017-02-27 10:16:29 +0100

    715 Refactor
    * Preparation for changes (parallel execution and rebuilding cache)
    * Maintenance announcement depending on environment (only required for live)

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,21 +1,22 @@
 import contextlib
+import os
 import time
 import urllib.request
-from os.path import join, dirname
 
 import envdir
 import configurations
 
+from fabric.context_managers import cd, prefix
 from fabric.contrib.files import exists
-from fabric.api import run, sudo, env
-from fabric.colors import green, yellow
+from fabric.api import run, env
+from fabric.colors import green
 from fabric.decorators import task
 from fabric.contrib import django
 from django.conf import settings
 
 # Load the django settings. This needs to read the env-variables and setup
 # django-configurations, before the settings_module can be accessed.
-envdir.read(join(dirname(dirname(__file__)), 'envs'))
+envdir.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'envs'))
 configurations.setup()
 django.settings_module('qcat.settings')
 
@@ -26,12 +27,14 @@ ENVIRONMENTS = {
         'label': 'dev',
         'host_string': settings.HOST_STRING_DEV,
         'touch_file': settings.TOUCH_FILE_DEV,
+        'use_deploy_announcement': False
     },
     'demo': {
         'branch': 'master',
         'label': 'live',
         'host_string': settings.HOST_STRING_DEMO,
         'touch_file': settings.TOUCH_FILE_DEMO,
+        'use_deploy_announcement': False,
         'url': 'https://qcat-demo.wocat.net/{}/wocat/list/?type=all',
     },
     'master': {
@@ -39,6 +42,7 @@ ENVIRONMENTS = {
         'label': 'live',
         'host_string': settings.HOST_STRING_LIVE,
         'touch_file': settings.TOUCH_FILE_LIVE,
+        'use_deploy_announcement': True,
         'url': 'https://qcat.wocat.net/{}/wocat/list/?type=all',
     },
     'common': {
@@ -90,100 +94,80 @@ def deploy(branch):
 
     for environment in BRANCH_HOSTINGS[branch]:
         set_environment(environment)
-        _set_maintenance_warning(env.source_folder)
-        _set_maintenance_mode(True, env.source_folder)
-        _get_latest_source(env.source_folder)
-        _update_virtualenv(env.source_folder)
-        _clean_static_folder(env.source_folder)
-        _update_static_files(env.source_folder)
-        _update_database(env.source_folder)
-        _set_maintenance_mode(False, env.source_folder)
-        _rebuild_configuration_cache()
+        if env.use_deploy_announcement:
+            _set_maintenance_warning()
+
+        _set_maintenance_mode(True)
+        _get_latest_source()
+        _update_virtualenv()
+        _clean_static_folder()
+        _update_static_files()
+        _update_database()
+        if _has_config_update_tag():
+            _reload_configuration_fixtures()
+            # also rebuild es index?
+            _delete_caches()
+        _set_maintenance_mode(False)
+        # _rebuild_configuration_cache()
+
         print(green("Everything OK"))
         _access_project()
-        _clean_sessions(env.source_folder)
-
-@task
-def provision(environment):
-    set_environment(environment)
-    _install_prerequirements()
-    _create_directory_structure(env.site_folder)
-    _get_latest_source(env.source_folder)
-    print(yellow("Provisioning completed. You may now create "
-                 "the local settings file!"))
+        _clean_sessions()
 
 
 @task
-def load_qcat_data(environment):
-    set_environment(environment)
-    run('cd {} && python manage.py load_qcat_data'.format(env.source_folder))
-
-
-@task
-def show_logs(environment, file='django.log', n=100):
+def show_logs(environment_name, file='django.log', n=100):
     """
     Arguments can be passed like fab develop show_logs:file=myfile.log,n=1
     """
-    set_environment(environment)
-    run('tail -n {n} {folder}/logs/{file}'.format(n=n, folder=env.source_folder, file=file))
+    set_environment(environment_name)
+    run('tail -n {n} {folder}/logs/{file}'.format(
+        n=n, folder=env.source_folder, file=file)
+    )
 
 
-def _install_prerequirements():
-    sudo('apt-get install apache2 git python3 python3-pip nodejs '
-         'nodejs-legacy npm')
-    sudo('npm install -g grunt-cli bower')
-    sudo('pip3 install virtualenv')
+def _get_latest_source():
+    with cd(env.source_folder):
+        run('git fetch')
+        run('git pull origin %(branch)s' % env)
 
 
-def _create_directory_structure(site_folder):
-    for subfolder in ('static', 'virtualenv', 'source'):
-        run('mkdir -p %s/%s' % (site_folder, subfolder))
+def _update_virtualenv():
+    with virtualenv():
+        run('pip3 install -r requirements/production.txt')
 
 
-def _get_latest_source(source_folder):
-    if exists(source_folder + '/.git'):
-        # Repository already there, fetch new commits
-        run('cd %s && git fetch' % (source_folder))
-    else:
-        # Create new repository
-        run('git clone %s %s' % (env.repo_url, source_folder))
-    # current_commit = local("git log -n 1 --format=%H", capture=True)
-    # run('cd %s && git reset --hard %s' % (source_folder, current_commit))
-    run('cd %s && git pull origin %s' % (source_folder, env.branch))
+def _clean_static_folder():
+    with cd(env.source_folder):
+        run('rm -r static/*')
 
 
-def _update_virtualenv(source_folder):
-    virtualenv_folder = source_folder + '/../virtualenv'
-    if not exists(virtualenv_folder + '/bin/pip'):
-        # Virtualenv does not yet exist
-        run('virtualenv --python=python3 %s' % virtualenv_folder)
-    run('%s/bin/pip3 install -r %s/requirements/production.txt'
-        % (virtualenv_folder, source_folder))
+def _update_static_files():
+    with cd(env.source_folder):
+        run('npm install &>/dev/null')
+        run('bower install | xargs echo')
+        run('grunt build:deploy --force')
+
+    _manage_py('collectstatic --noinput')
+    _manage_py('compress --force')
 
 
-def _clean_static_folder(source_folder):
-    run('cd %s && rm -r static/*' % source_folder)
+def _update_database():
+    _manage_py('migrate --noinput')
+    _manage_py('load_qcat_data')
 
 
-def _update_static_files(source_folder):
-    run('cd %s && npm install &>/dev/null' % source_folder)
-    run('cd %s && bower install | xargs echo' % source_folder)
-    run('cd %s && grunt build:deploy --force' % source_folder)
-    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput'
-        % source_folder)
-    run('cd %s && ../virtualenv/bin/python3 manage.py compress --force ' %
-        source_folder)
+def _has_config_update_tag():
+    # read git tags of latest commit and check if a 'flag' tag is set.
+    return True
 
 
-def _update_database(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput'
-        % (source_folder))
-    run('cd %s && ../virtualenv/bin/python3 manage.py load_qcat_data'
-        % (source_folder))
+def _reload_configuration_fixtures():
+    _manage_py('loaddata global_key_values technologies approaches cca watershed')
 
 
-def _reload_apache(site_folder):
-    run('cd %s && touch wsgi/wsgi.py' % site_folder)
+def _delete_caches():
+    pass
 
 
 def _reload_uwsgi():
@@ -191,21 +175,16 @@ def _reload_uwsgi():
     run('touch {}'.format(env.touch_file))
 
 
-def _set_maintenance_mode(value, source_folder):
+def _set_maintenance_mode(value):
     # Toggle maintenance mode on or off. This will reload apache!
     run('echo {bool_value} > {envs_file}'.format(
         bool_value=str(value),
-        envs_file=join(source_folder, 'envs', 'MAINTENANCE_MODE')))
+        envs_file=os.path.join(env.source_folder, 'envs', 'MAINTENANCE_MODE')))
     # There were issues with permissions, so the lock-file remained in place.
     # Prevent this from happening again.
     if exists(settings.MAINTENANCE_LOCKFILE_PATH):
         run('rm {}'.format(settings.MAINTENANCE_LOCKFILE_PATH))
     _reload_uwsgi()
-
-
-def _rebuild_configuration_cache():
-    run('cd %s && ../virtualenv/bin/python3 manage.py build_config_caches' %
-        env.source_folder)
 
 
 def _access_project():
@@ -214,12 +193,13 @@ def _access_project():
     """
     if hasattr(env, 'url'):
         for lang in settings.LANGUAGES:
-            with contextlib.closing(urllib.request.urlopen(env.url.format(lang[0]))) as request:
+            url = urllib.request.urlopen(env.url.format(lang[0]))
+            with contextlib.closing(url) as request:
                 request.read()
                 print('Read response from: {}'.format(request.url))
 
 
-def _set_maintenance_warning(source_folder):
+def _set_maintenance_warning():
     """
     Activate the maintenance warning and wait until the deploy timeout is over,
     giving people the chance to save their work.
@@ -228,14 +208,24 @@ def _set_maintenance_warning(source_folder):
     always required and are built on human interaction. They also are relatively
     expensive and don't need to be executed on each deploy.
     """
-    run('cd %s && ../virtualenv/bin/python3 manage.py '
-        'set_next_maintenance' % source_folder)
+    _manage_py('set_next_maintenance')
     time.sleep(settings.DEPLOY_TIMEOUT)
 
 
-def _clean_sessions(source_folder):
+def _clean_sessions():
     """
     This should be in a crontab.
     """
-    run('cd %s && ../virtualenv/bin/python3 manage.py '
-        'clearsessions' % source_folder)
+    _manage_py('clearsessions')
+
+
+def _manage_py(command):
+    with virtualenv():
+        run('python3 manage.py %s' % command)
+
+
+@contextlib.contextmanager
+def virtualenv():
+    with prefix('source %(site_folder)s/virtualenv/bin/activate' % env):
+        with cd(env.source_folder):
+            yield
