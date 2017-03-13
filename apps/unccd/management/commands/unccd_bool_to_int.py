eedf793abb8c75ff358a7afb6cf58ec54fe4f5b6
Lukas Vonlanthen <lukas.vonlanthen@gmail.com>
2017-03-13 14:21:19 +0100

    Store UNCCD boolean values as 1/0.
    * Change management command to store boolean values as 1 or 0 instead of True or False.

--- apps/unccd/management/commands/unccd_bool_to_int.py
+++ apps/unccd/management/commands/unccd_bool_to_int.py
@@ -14,7 +14,7 @@ class Command(NoArgsCommand):
     field type is derived from the first element. For the unccd cases, this
     causes problems, as some boolen values are stored as 'true'/'false' and some
     as '1'/'0'. This command harmonizes this by setting all data to
-    'true'/'false'.
+    1/0.
 
     This is not efficient or nice, but needs to be run just once.
     """
@@ -23,8 +23,11 @@ class Command(NoArgsCommand):
             prevent_updates_on_published_items, sender=Questionnaire
         )
 
-        config = get_configuration('unccd')
-        questionnaires = Questionnaire.objects.filter(code__startswith='unccd')
+        configuration_code = 'unccd'
+
+        config = get_configuration(configuration_code)
+        questionnaires = Questionnaire.objects.filter(
+            code__startswith=configuration_code)
         self.find_int_values(questionnaires, config)
 
         pre_save.connect(
@@ -36,7 +39,7 @@ class Command(NoArgsCommand):
             for qg_keyword, question_group in questionnaire.data.items():
                 for questions in question_group:
                     for question_keyword, value in questions.items():
-                        if value in ['1', 1, '0', 0]:
+                        if value is True or value is False:
                             question_config = config.get_question_by_keyword(
                                 questiongroup_keyword=qg_keyword,
                                 keyword=question_keyword
@@ -44,14 +47,15 @@ class Command(NoArgsCommand):
                             if question_config.field_type == 'bool':
                                 self.set_value(
                                     questionnaire, qg_keyword, question_keyword,
-                                    self.get_bool(value)
+                                    self.get_int(value)
                                 )
 
-    def get_bool(self, value):
-        return True if value in [1, '1'] else False
+    def get_int(self, value):
+        return 1 if value is True else 0
 
     def set_value(self, questionnaire, qg_keyword, question_keyword, value):
-        if question_keyword not in questionnaire.data[qg_keyword][0]:
-            raise Exception
-        questionnaire.data[qg_keyword][0][question_keyword] = value
+        for questiongroup_data in questionnaire.data[qg_keyword]:
+            if question_keyword not in questiongroup_data:
+                raise Exception
+            questiongroup_data[question_keyword] = value
         questionnaire.save()
