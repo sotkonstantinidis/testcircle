52994ebdcb2b494a5b32493625ee9b607f3dcf3c
Sebastian Manger <s.manger@gmx.ch>
2018-03-12 15:23:16 +0100

    1775 Increase memory usage with by requesting different languages

--- stress_tests/locustfile.py
+++ stress_tests/locustfile.py
@@ -10,55 +10,50 @@ class UserBehavior(TaskSet):
     """
     def on_start(self):
         # maybe: use environment variables
-        self.approaches = []
-        self.technologies = []
-        self.user_id = 0
-        self.credentials = {}
+        self.approaches = ['approaches_490']
+        self.technologies = ['technologies_784']
+        self.user_id = 3088
+        self.languages = ['en', 'fr', 'es', 'ru', 'km', 'lo', 'ar', 'pt', 'af']
 
-    @task(1)
-    def login(self):
-        """
-        Check the login, incl. the 'API'
-        """
-        response = self.client.get('/en/accounts/login/')
-        credentials = self.credentials
-        credentials['csrfmiddlewaretoken'] = response.cookies['csrftoken']
-        self.client.headers['Referer'] = self.client.base_url
-        self.client.post('/en/accounts/login/', credentials)
+    @property
+    def language(self):
+        return '/{language}'.format(language=random.choice(self.languages))
 
     @task(2)
     def start_edit(self):
         """
         Check the configuration and lru_cache
         """
-        self.login()
-        self.client.get('/en/wocat/technologies/edit/new/')
+        self.client.get('{}/wocat/technologies/edit/new/'.format(self.language))
 
     @task(10)
     def index(self):
         self.client.get('/')
 
+    @task(20)
+    def index_list(self):
+        self.client.get('{}/wocat/list/'.format(self.language))
+
     @task(3)
-    def my_slm_data(self):
+    def user_profile(self):
         """
         Check the db
         """
-        self.login()
-        self.client.get('/en/accounts/user/{}/questionnaires/'.format(self.user_id))
+        self.client.get('{}/accounts/user/{}/'.format(self.language, self.user_id))
 
     @task(5)
     def approach(self):
         """
         Check cache, db (initial request)
         """
-        self.client.get('/en/wocat/approaches/view/approaches_{}'.format(random.choice(self.approaches)))
+        self.client.get('{}/wocat/approaches/view/{}'.format(self.language, random.choice(self.approaches)))
 
     @task(5)
     def technology(self):
         """
         Check cache, db (initial request)
         """
-        self.client.get('/en/wocat/approaches/view/technologies_{}'.format(random.choice(self.technologies)))
+        self.client.get('{}/wocat/approaches/view/{}'.format(self.language, random.choice(self.technologies)))
 
 
 class WebsiteUser(HttpLocust):
